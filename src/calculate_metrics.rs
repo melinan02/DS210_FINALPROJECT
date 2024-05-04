1729c90f3697ec8cff04d992b10635da675f17d5
melinan02 <melinan@bu.edu>
2024-05-04 15:06:23 -0400

    Commit changes to modules

--- src/calculate_metrics.rs
+++ src/calculate_metrics.rs
@@ -1,10 +1,47 @@
+use petgraph::graph::{Graph, UnGraph};
 use petgraph::prelude::*;
-use petgraph::algo::{average_distance, betweenness_centrality};
+use petgraph::algo::{dijkstra, eccentricity, unweighted_shortest_path};
+use petgraph::visit::NodeCount;
 
 pub fn calculate_average_distance(graph: &UnGraph<usize, ()>) -> Option<f64> {
-    average_distance(&graph)
+    let n = graph.node_count();
+    let mut total_distance = 0;
+    let mut total_pairs = 0;
+
+    for node in graph.node_indices() {
+        let distances = dijkstra(&graph, node, None, |_| 1);
+        for (_, distance) in distances {
+            if let Option(d) = distance {
+                total_distance += d;
+                total_pairs += 1;
+            }
+        }
+    }
+
+    if total_pairs > 0 {
+        Some(total_distance as f64 / total_pairs as f64)
+    } else {
+        None
+    }
 }
 
 pub fn calculate_betweenness_centrality(graph: &UnGraph<usize, ()>) -> Vec<f64> {
-    betweenness_centrality(&graph, None)
+    let n = graph.node_count();
+    let mut betweenness = vec![0.0; n];
+
+    for node in graph.node_indices() {
+        let shortest_paths = unweighted_shortest_path(&graph, node);
+        for target in graph.node_indices() {
+            if node != target {
+                for path in shortest_paths.paths_to(target) {
+                    for edge in path {
+                        betweenness[edge] += 1.0;
+                    }
+                }
+            }
+        }
+    }
+
+    let normalization_factor = (n - 1) * (n - 2) / 2;
+    betweenness.iter_mut().map(|x| *x / normalization_factor as f64).collect()
 }
