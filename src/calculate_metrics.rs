275aae64a6885d3b00af0b11b241003ba3321a0b
melinan02 <melinan@bu.edu>
2024-05-04 15:39:22 -0400

    Merge branch 'main' of https://github.com/melinan02/DS210_FINALPROJECT

--- src/calculate_metrics.rs
+++ src/calculate_metrics.rs
@@ -1,7 +1,5 @@
-use petgraph::graph::{Graph, UnGraph};
-use petgraph::prelude::*;
-use petgraph::algo::{dijkstra, eccentricity, unweighted_shortest_path};
-use petgraph::visit::NodeCount;
+use petgraph::{algo::*, prelude::*};
+use petgraph::graph::NodeIndex;
 
 pub fn calculate_average_distance(graph: &UnGraph<usize, ()>) -> Option<f64> {
     let n = graph.node_count();
@@ -11,8 +9,8 @@ pub fn calculate_average_distance(graph: &UnGraph<usize, ()>) -> Option<f64> {
     for node in graph.node_indices() {
         let distances = dijkstra(&graph, node, None, |_| 1);
         for (_, distance) in distances {
-            if let Option(d) = distance {
-                total_distance += d;
+            if let Some(d) = distance {
+                total_distance += d as i32; // Convert distance to usize or the appropriate integer type
                 total_pairs += 1;
             }
         }
@@ -30,18 +28,35 @@ pub fn calculate_betweenness_centrality(graph: &UnGraph<usize, ()>) -> Vec<f64> 
     let mut betweenness = vec![0.0; n];
 
     for node in graph.node_indices() {
-        let shortest_paths = unweighted_shortest_path(&graph, node);
-        for target in graph.node_indices() {
-            if node != target {
-                for path in shortest_paths.paths_to(target) {
-                    for edge in path {
-                        betweenness[edge] += 1.0;
+        let mut paths: Vec<Vec<NodeIndex>> = Vec::new();
+
+        // Perform breadth-first search (BFS) from the current node
+        let mut queue = vec![vec![node]];
+        while let Some(path) = queue.pop() {
+            let current_node = *path.last().unwrap();
+            let neighbors: Vec<NodeIndex> = graph.neighbors(current_node).collect();
+
+            for &neighbor in &neighbors {
+                if !path.contains(&neighbor) {
+                    let mut new_path = path.clone();
+                    new_path.push(neighbor);
+                    queue.push(new_path);
+                    if neighbor != node {
+                        paths.push(new_path);
                     }
                 }
             }
         }
+
+        // Count the number of shortest paths passing through each edge
+        for path in paths {
+            for &node in &path {
+                betweenness[node.index()] += 1.0;
+            }
+        }
     }
 
+    // Normalize betweenness centrality scores
     let normalization_factor = (n - 1) * (n - 2) / 2;
     betweenness.iter_mut().map(|x| *x / normalization_factor as f64).collect()
 }
